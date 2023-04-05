5c1a6b38a32c86aa850f39aa4f8fcc6d42a23b72
Praddyumn Yadav <praddyumny@gmail.com>
2023-04-05 19:14:49 +0530

    Formatted the code using black.

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 # Create our main Function
 def main():
-	# Getting the Master Password
-	pwd = input("Enter the Master Password: ")
+    # Getting the Master Password
+    pwd = input("Enter the Master Password: ")
 
-	# Printing the Mode Message
-	print(
-		"""
+    # Printing the Mode Message
+    print(
+        """
 		Mode: 
 			Add a new Password: add
 			View Existing Passwords: view
 			Press q to Quit.
 		"""
-	)
+    )
 
-	# Getting the mode and do Operations
-	while True:
-		mode = input("Mode: ").lower()
-		if mode == "q":
-			break
-		elif mode == "view":
-			pass
-		elif mode == "add":
-			pass
-		else:
-			print("Invalid Input")
-			continue
+    # Getting the mode and do Operations
+    while True:
+        mode = input("Mode: ").lower()
+        if mode == "q":
+            break
+        elif mode == "view":
+            pass
+        elif mode == "add":
+            pass
+        else:
+            print("Invalid Input")
+            continue
 
 
 # Run Our main() function
