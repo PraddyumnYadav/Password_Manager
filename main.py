50b01edfc12ad4365836c344428064885c7bcb67
Praddyumn Yadav <praddyumny@gmail.com>
2023-04-05 19:11:26 +0530

    Wrap up Our Programm in a main() function.

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-# Getting the Master Password
-pwd = input("Enter the Master Password: ")
+# Create our main Function
+def main():
+	# Getting the Master Password
+	pwd = input("Enter the Master Password: ")
 
-# Printing the Mode Message
-print(
-	"""
-	Mode: 
-		Add a new Password: add
-		View Existing Passwords: view
-		Press q to Quit.
-	"""
-)
+	# Printing the Mode Message
+	print(
+		"""
+		Mode: 
+			Add a new Password: add
+			View Existing Passwords: view
+			Press q to Quit.
+		"""
+	)
 
-# Getting the mode and do Operations
-while True:
-	mode = input("Mode: ").lower()
-	if mode == "q":
-		break
-	elif mode == "view":
-		pass
-	elif mode == "add":
-		pass
-	else:
-		print("Invalid Input")
-		continue
+	# Getting the mode and do Operations
+	while True:
+		mode = input("Mode: ").lower()
+		if mode == "q":
+			break
+		elif mode == "view":
+			pass
+		elif mode == "add":
+			pass
+		else:
+			print("Invalid Input")
+			continue
+
+
+# Run Our main() function
+main()
