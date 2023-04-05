af1a7f568c01e3c1f236a20626c4b446d8da73d0
Praddyumn Yadav <praddyumny@gmail.com>
2023-04-05 19:00:47 +0530

    Get MasterPassword and Mode from the user.

---
+++ main.py
@@ -0,0 +1,12 @@
+# Getting the Master Password
+pwd = input("Enter the Master Password: ")
+
+# Getting the mode
+print(
+	"""
+	Mode: 
+		Add a new Password: add
+		View Existing Passwords: view
+	"""
+)
+mode = input("Mode: ")
