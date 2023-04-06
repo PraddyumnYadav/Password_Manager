55352a0aa3ef2c736955cb580f2873b690b7b496
Praddyumn Yadav <praddyumny@gmail.com>
2023-04-06 15:28:28 +0530

    Define Models in models.py

--- Password_Manager_Django/password_app/models.py
+++ Password_Manager_Django/password_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Password(models.Model):
+    account_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    encrypted_password = models.CharField(max_length=200)
